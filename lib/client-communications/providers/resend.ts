84487c655b8185c895f57c907ef2c9c0e8f2476b
Helios Real Estate Media <heliosrealestatemedia@gmail.com>
2026-08-03 13:23:06 -0600

    Use valid Resend tags for bulk delivery

--- lib/client-communications/providers/resend.ts
+++ lib/client-communications/providers/resend.ts
@@ -9,6 +9,7 @@ import {
   normalizeResendError,
   providerAdminMessage,
   resolveDeliveryConfig,
+  resendTagValue,
   RESEND_TIMEOUT_MS,
   type EmailProviderErrorCode,
   type NormalizedProviderError,
@@ -194,7 +195,7 @@ export async function sendEmailBatch(input: {
       replyTo: replyTo || undefined,
       subject: message.subject,
       html: message.html,
-      tags: [{ name: "campaign_id", value: input.campaignId }],
+      tags: [{ name: "campaign_id", value: resendTagValue(input.campaignId) }],
       headers: message.unsubscribeUrl ? oneClickUnsubscribeHeaders(message.unsubscribeUrl) : undefined,
     })), { idempotencyKey }), metadata);
     const accepted = response.data?.data;
